4ce628089c0de9786dfecb4027b08c7e671bd99c
Kurt Lourens <hi@kurtlourens.com>
2022-09-26 18:04:23 +0200

    📦 Generate AssistantApps data

---
+++ lib/constants/AppAvailableLanguages.dart
@@ -0,0 +1,33 @@
+
+import 'package:assistantapps_flutter_common/assistantapps_flutter_common.dart';
+
+List<LocalizationMap> supportedLanguageMaps = [
+      LocalizationMap(LocaleKey.english, 'en', 'gb', percentageComplete: 100),
+    LocalizationMap(LocaleKey.dutch, 'nl', 'nl', percentageComplete: 75),
+    LocalizationMap(LocaleKey.german, 'de', 'de', percentageComplete: 96),
+    LocalizationMap(LocaleKey.french, 'fr', 'fr', percentageComplete: 96),
+    LocalizationMap(LocaleKey.italian, 'it', 'it', percentageComplete: 90),
+    LocalizationMap(LocaleKey.russian, 'ru', 'ru', percentageComplete: 95),
+    LocalizationMap(LocaleKey.polish, 'pl', 'pl', percentageComplete: 92),
+    LocalizationMap(LocaleKey.brazilianPortuguese, 'pt-br', 'br', percentageComplete: 91),
+    LocalizationMap(LocaleKey.portuguese, 'pt', 'pt', percentageComplete: 35),
+    LocalizationMap(LocaleKey.norwegian, 'no', 'no', percentageComplete: 85),
+    LocalizationMap(LocaleKey.romanian, 'ro', 'ro', percentageComplete: 42),
+    LocalizationMap(LocaleKey.spanish, 'es', 'es', percentageComplete: 96),
+    LocalizationMap(LocaleKey.czech, 'cs', 'cz', percentageComplete: 90),
+    LocalizationMap(LocaleKey.turkish, 'tr', 'tr', percentageComplete: 90),
+    LocalizationMap(LocaleKey.hungarian, 'hu', 'hu', percentageComplete: 90),
+    LocalizationMap(LocaleKey.simplifiedChinese, 'zh-hans', 'cn', percentageComplete: 90),
+    LocalizationMap(LocaleKey.traditionalChinese, 'zh-hant', 'cn', percentageComplete: 60),
+    LocalizationMap(LocaleKey.arabic, 'ar', 'ar', percentageComplete: 28),
+    LocalizationMap(LocaleKey.vietnamese, 'vi-vn', 'vn', percentageComplete: 90),
+    LocalizationMap(LocaleKey.urdu, 'ur', 'pk', percentageComplete: 16),
+    LocalizationMap(LocaleKey.filipino, 'ph', 'ph', percentageComplete: 2),
+    LocalizationMap(LocaleKey.indonesian, 'id', 'id', percentageComplete: 89),
+    LocalizationMap(LocaleKey.malaysian, 'ms', 'my', percentageComplete: 89),
+    LocalizationMap(LocaleKey.tagalog, 'tl', 'ph', percentageComplete: 0),
+    LocalizationMap(LocaleKey.japanese, 'ja', 'jp', percentageComplete: 5),
+    LocalizationMap(LocaleKey.afrikaans, 'af', 'za', percentageComplete: 39),
+
+];
+
