383ef5e2027d449efdac6239b8ee47675c692ec1
Kurt Lourens <hi@kurtlourens.com>
2022-10-28 14:05:25 +0200

    📝 Update WhatIsNew Guid

--- lib/env/assistant_apps_settings.dart
+++ lib/env/assistant_apps_settings.dart
@@ -1,2 +1,2 @@
 const assistantAppsAppGuid = 'e9fb4e7e-79d8-c5a2-79fa-e206b0681780';
-const currentWhatIsNewGuid = '675dc67b-a927-458f-9d46-8930864280df';
+const currentWhatIsNewGuid = '0989a22f-a013-4021-9b7d-8495300732d3';
