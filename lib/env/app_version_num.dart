fec11ae4098c6c9b63ecfba7612e015f48f12bb4
Kurt Lourens <kurt.lourens@outlook.com>
2023-03-08 00:27:44 +0100

    🔖 0.0.6

--- lib/env/app_version_num.dart
+++ lib/env/app_version_num.dart
@@ -1,3 +1,3 @@
-const appsBuildNum = 5;
-const appsBuildName = '0.0.5';
-const appsCommit = '16cd9a31e91583bb4ff79b6f7c641ee9b3439b2c';
+const appsBuildNum = 6;
+const appsBuildName = '0.0.6';
+const appsCommit = '9242532fcc55fb910402731199674b0772d4867b';
--- lib/env/assistant_apps_settings.dart
+++ lib/env/assistant_apps_settings.dart
@@ -1,2 +1,2 @@
 const assistantAppsAppGuid = 'e9fb4e7e-79d8-c5a2-79fa-e206b0681780';
-const currentWhatIsNewGuid = '8ac8a244-9f95-4292-a643-525d3de831b6';
+const currentWhatIsNewGuid = 'a54006c9-7987-44dd-a8c7-6953c3445a70';
