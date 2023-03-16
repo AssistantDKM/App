74b7af87d59aecdf29a72c75a9336f028ec5dad3
Kurt Lourens <hi@kurtlourens.com>
2023-03-16 19:24:22 +0100

    📝 Run version script

--- lib/env/app_version_num.dart
+++ lib/env/app_version_num.dart
@@ -1,3 +1,3 @@
 const appsBuildNum = 6;
 const appsBuildName = '0.0.6';
-const appsCommit = '9242532fcc55fb910402731199674b0772d4867b';
+const appsCommit = '4a96a1d05c19f5b4376c9ea63df9fc2dbd79941a';
