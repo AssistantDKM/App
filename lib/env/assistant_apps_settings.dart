ac4bcb2a36ba4274a3b251c351a5c7351a83d47b
Kurt Lourens <hi@kurtlourens.com>
2023-04-19 16:37:22 +0200

    🔖 0.0.9

--- lib/env/assistant_apps_settings.dart
+++ lib/env/assistant_apps_settings.dart
@@ -1,2 +1,2 @@
 const assistantAppsAppGuid = 'e9fb4e7e-79d8-c5a2-79fa-e206b0681780';
-const currentWhatIsNewGuid = '194f6be0-5f66-47c3-ab89-b48dc6874972';
+const currentWhatIsNewGuid = '9333906b-3262-438c-b03f-5931c87de295';
