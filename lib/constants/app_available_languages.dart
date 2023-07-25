529b966f0fcc6ab090961c0c3fc4b141414ff822
Kurt Lourens <kurt.lourens@outlook.com>
2023-07-25 16:03:58 +0200

    📦 Regen compiled JSON (2)

--- lib/constants/app_available_languages.dart
+++ lib/constants/app_available_languages.dart
@@ -3,7 +3,7 @@ import 'package:assistantapps_flutter_common/assistantapps_flutter_common.dart';
 
 List<LocalizationMap> supportedLanguageMaps = [
       LocalizationMap(LocaleKey.english, 'en', 'gb', percentageComplete: 100),
-    LocalizationMap(LocaleKey.french, 'fr', 'fr', percentageComplete: 73),
+    LocalizationMap(LocaleKey.french, 'fr', 'fr', percentageComplete: 74),
     LocalizationMap(LocaleKey.russian, 'ru', 'ru', percentageComplete: 72),
     LocalizationMap(LocaleKey.simplifiedChinese, 'zh-hans', 'cn', percentageComplete: 69),
     LocalizationMap(LocaleKey.traditionalChinese, 'zh-hant', 'cn', percentageComplete: 42),
